2b66e209152be4a71d0f5f45144f1a943050e67a
Niraj Choudhary <nirajchoudhary79@yahoo.in>
2024-09-16 01:42:05 +0530

    Templates

--- dataentry_app/views.py
+++ dataentry_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from . utils import get_all_custom_models
+
+
+
+from django.http import HttpResponse
 
 # Create your views here.
+
+def import_data(request):
+
+    if request.method == 'POST':
+        return
+    else:
+        custom_models = get_all_custom_models()
+        context = {'custom_models': custom_models}
+        return render(request,'dataentry_app/dataentry.html', context)
+
+
